eed42e242467261422ae29bc6138164f4d4fbd06
Tejas Gawande <tejugawande25@gmail.com>
2024-01-31 01:10:22 +0530

    ui

--- chatbot-frontend/src/App.js
+++ chatbot-frontend/src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import "@chatscope/chat-ui-kit-styles/dist/default/styles.min.css";
+import {MainContainer,ChatContainer,MessageList,Message,MessageInput,TypingIndicator} from "@chatscope/chat-ui-kit-react"
 
 function App() {
+  const[messages, setMessages] = useState([
+   {
+    meassge:"Hello, I m chatGPT!",
+    sender:"chatGPT"
+   }
+  ])
+
+  const handleSend = async(message) =>{
+    const newMessage = {
+      meassge:message,
+      sender:"user",
+      direction:"outgoing"
+    }
+    const newMessages = [...messages, newMessage] // all the old messages  + new messages
+
+    //update our message state
+    setMessages(newMessages);
+
+    //  process message to chatGPT (send it over and see the results)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{display:"flex",justifyContent:"center",marginTop:"50px"}}>
+     <div style={{position:"relative",height:"600px", width:"500px"}}>
+      <MainContainer>
+        <ChatContainer>
+          <MessageList>
+           {messages.map((message,i) =>{
+              console.log(message);
+              return <Message key={i} model={message}  />
+           })}
+          </MessageList>
+          <MessageInput 
+          placeholder="Type message here.."
+          onSend={handleSend}
+          />
+
+         
+        </ChatContainer>
+      </MainContainer>
+     </div>
     </div>
   );
 }
